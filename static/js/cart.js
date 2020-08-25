37223ab6285e892bb359bef2411bf285667aba4f
hashik <mohamedhashik.kp@gmail.com>
2020-08-25 21:08:20 +0530

    js complete

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,20 +8,48 @@ for( var i=0;i < updateBtns.length; i++){
 
         console.log('USER:',user)
         if( user == "AnonymousUser"){
-                 console.log('User is not authenticated')
+                 addCookieItem(productId,action)
          }else{
                updateUserOrder(productId,action)
         }
     })
 }
+function addCookieItem(productId, action)
+{
+     console.log('Not logged in..')
+     if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] ={'quantity':1}
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+	location.reload()
+}
+
 function updateUserOrder(productId,action)
-{ console.log('User is  authenticated, sending data')
+{  console.log('User is logged in,sending data')
       var url = '/update_item/'
 
-      fetch(url,{
+
+      fetch(url,
+      {
         method:'POST',
         headers:{
-            'Content-Type':'application/json'
+            'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken,
         },
         body:JSON.stringify({'productId': productId, 'action':action})
       })
@@ -29,6 +57,8 @@ function updateUserOrder(productId,action)
          return response.json()
       })
       .then((data) =>{
-         console.log('data:',data)
+           console.log('data:',data)
+           location.reload()
+
       })
-        }
+   }
